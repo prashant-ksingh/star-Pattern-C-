d95b6b70670cec8e2fcdd190e621ac7c18ee119b
unknown <prashantkumarsingh934@gmail.com>
2023-11-24 23:45:47 +0530

    pyramid pattern

--- pattern13.cpp
+++ pattern13.cpp
@@ -3,3 +3,20 @@
 //   * * *
 //  * * * * 
 // * * * * *
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n=5;
+    for(int row =1; row<= n; row++)
+    {
+        for(int k= n-row; k>=1; k--)
+        cout<<" ";
+        for(int col = 1; col<= row; col++)
+        {
+            cout<<"* ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}

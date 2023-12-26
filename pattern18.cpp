1cc78c12c7fa50493e1fa2dde5687f42b0ad02b6
unknown <prashantkumarsingh934@gmail.com>
2023-12-26 23:44:54 +0530

    pattern18

---
+++ pattern18.cpp
@@ -0,0 +1,22 @@
+// Rohmbus
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n=5;
+    for(int i =0; i<=n;i++)
+    {
+        for(int k=1; k<=i; k++)
+        cout<<" ";
+        for(int j =0; j<=n; j++ )
+        {
+            cout<<"* ";
+        }
+        cout<<endl;
+    }
+
+    return 0;
+} 

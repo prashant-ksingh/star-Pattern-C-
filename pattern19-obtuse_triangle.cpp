a1573f6dae056f8d8165bf51067070b09a4b1ad4
unknown <prashantkumarsingh934@gmail.com>
2023-12-26 23:52:18 +0530

    pattern19

---
+++ pattern19-obtuse_triangle.cpp
@@ -0,0 +1,24 @@
+
+
+// Obtuse Triangle
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n=5;
+    for(int i =0; i<=n;i++)
+    {
+        for(int k=1; k<=i; k++)
+        cout<<" ";
+        for(int j =0; j<=i; j++ )
+        {
+            cout<<"* ";
+        }
+        cout<<endl;
+    }
+
+    return 0;
+}
